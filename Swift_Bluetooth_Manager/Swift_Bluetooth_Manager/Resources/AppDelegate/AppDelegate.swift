aa05330bf4b491ba04dd3b70751270b7c98e9531
Dima <dima.chumakovwork@gmail.com>
2021-08-17 09:03:57 +0300

    initial commit

---
+++ Swift_Bluetooth_Manager/Swift_Bluetooth_Manager/Resources/AppDelegate/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  Swift_Bluetooth_Manager
+//
+//  Created by Dmytro Chumakov on 17.08.2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
---
+++ Swift_Bluetooth_Manager/Swift_Bluetooth_ManagerUITests/Swift_Bluetooth_ManagerUITests.swift
@@ -0,0 +1,12 @@
+//
+//  Swift_Bluetooth_ManagerUITests.swift
+//  Swift_Bluetooth_ManagerUITests
+//
+//  Created by Dmytro Chumakov on 17.08.2021.
+//
+
+import XCTest
+
+class Swift_Bluetooth_ManagerUITests: XCTestCase {
+
+}
